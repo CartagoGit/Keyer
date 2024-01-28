d86d1ccf9ab664514dd556b4490d98228e28dea7
Cartago <cartago.relaxingcup@gmail.com>
2024-01-28 06:52:47 +0100

    Remove keyer.js and update pack:online command

---
+++ bin/index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+console.log('Keyer starting...');
+require('./keyer.js');
--- bin/keyer.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-console.log('Keyer starting...');
-require('ts-node').register();
-import '../src/keyer.ts';
