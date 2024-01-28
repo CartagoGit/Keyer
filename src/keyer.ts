aaae0c3e549b1a8fd7e1224a1d55444f65a03eb3
Cartago <cartago.relaxingcup@gmail.com>
2024-01-28 04:26:35 +0100

    Update keyer version and add pack script

--- src/keyer.ts
+++ src/keyer.ts
@@ -1,56 +1,49 @@
 import { createInterface } from 'node:readline';
-import { readFileSync, writeFileSync } from 'node:fs';
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'node:fs';
+import { dirname } from 'node:path';
 import { decryptAny, encryptAny } from './index';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 
 // Keyer CLI
-yargs(hideBin(process.argv)).command(
-	'$0',
-	'Keyer library commands',
-	(yargs) => {
-		return yargs
-			.option('env-file', {
-				alias: 'ef',
-				description: 'Route where is the file for encrypting',
-				type: 'string',
-			})
-			.option('hash-file', {
-				alias: 'hf',
-				description: 'Route for the new hash file',
-				type: 'string',
-			})
-			.option('version', {
-				alias: 'v',
-				description: 'Library version',
-				type: 'boolean',
-			})
-			.option('help', {
-				alias: 'h',
-				description: 'Library help',
-				type: 'boolean',
-			});
-	},
-	(argv) => {
-		if (argv.version) {
-			console.log('Version: ' + process.env.npm_package_version);
-		} else if (argv.help) {
-			console.log('Keyer Library Help');
-			console.log('Commands:');
-			console.log('  -v, --version: Library version');
-			console.log('  -h, --help: Library help');
-			console.log('Options:');
-			console.log(
-				"  --env-file, -ef: Route where is the file for encrypting. Default: '.env'"
-			);
-			console.log(
-				"  --hash-file, -hf: Route for the new hash file. Default: 'keyer/encrypted-hash.txt'"
-			);
-		} else {
-			script({ envArg: argv.envFile, hashArg: argv.hashFile });
+const createCli = () => {
+	yargs(hideBin(process.argv)).command(
+		'$0',
+		'Keyer library commands',
+		(yargs) => {
+			return yargs
+				.version(false)
+				.option('env-file', {
+					alias: 'ef',
+					description: 'Route where is the file for encrypting',
+					type: 'string',
+				})
+				.option('hash-file', {
+					alias: 'hf',
+					description: 'Route for the new hash file',
+					type: 'string',
+				})
+				.option('version', {
+					alias: 'v',
+					description: 'Library version',
+					type: 'boolean',
+				})
+				.option('help', {
+					alias: 'h',
+					description: 'Library help',
+					type: 'boolean',
+				});
+		},
+		(argv) => {
+			if (argv.version)
+				console.log(
+					'Keyer version: ' + require('../package.json').version
+				);
+			else if (argv.help) console.log('Keyer help:');
+			else script({ envArg: argv.envFile, hashArg: argv.hashFile });
 		}
-	}
-).argv;
+	).argv;
+};
 
 // Keyer script
 const script = (props: { envArg?: string; hashArg?: string }) => {
@@ -83,7 +76,19 @@ const script = (props: { envArg?: string; hashArg?: string }) => {
 						secretSalt: salt,
 						toEncrypt: envs,
 					});
-					writeFileSync(hashFile, encrypted);
+					// Verifica si el archivo existe
+					if (!existsSync(hashFile)) {
+						// Verifica si el directorio existe
+						const dir = dirname(hashFile);
+						if (!existsSync(dir)) {
+							// Crea el directorio
+							mkdirSync(dir, { recursive: true });
+						}
+					}
+					writeFileSync(hashFile, encrypted, {
+						encoding: 'utf-8',
+						flag: 'w',
+					});
 				} else if (keyKindCrypto === 'd') {
 					// Decrypt hash file and show envs in console
 					const hash = readFileSync(hashFile, 'utf-8');
@@ -94,3 +99,5 @@ const script = (props: { envArg?: string; hashArg?: string }) => {
 		}
 	);
 };
+
+createCli();
