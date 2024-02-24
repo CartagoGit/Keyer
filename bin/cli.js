b66065eb37aac57ec8c254f0a2192b9d32260676
Cartago <cartago.relaxingcup@gmail.com>
2024-02-24 23:18:24 +0100

    Revert "Eliminados archivos y módulos innecesarios"
    This reverts commit 6e844ceb74f5aac1ec78dd3ff06b70fa03e90b2e.

---
+++ bin/cli.js
@@ -0,0 +1,54 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createCli = void 0;
+const commander_1 = require("commander");
+const package_json_1 = __importDefault(require("../package.json"));
+const _1 = require(".");
+const cli_props_1 = require("./cli-props");
+const keyer_interface_1 = require("./interfaces/keyer.interface");
+// Start CLI
+const createCli = () => {
+    const program = new commander_1.Command();
+    program
+        .name('keyer')
+        .description(package_json_1.default.description)
+        .version(package_json_1.default.version, '-v', 'output Keyer current version')
+        .showHelpAfterError(cli_props_1.helpMessage)
+        .helpOption('-h, --help', 'output Keyer help')
+        .action((args) => {
+        const firstCommand = program.args[0];
+        if (!!firstCommand && !keyer_interface_1.availableCommands.includes(firstCommand)) {
+            console.error(`error: unrecognized command: ${firstCommand}\n${cli_props_1.helpMessage}`);
+            process.exit(1);
+        }
+        (0, _1.keyerCommand)(args);
+    });
+    for (const option of Object.values(cli_props_1.keyerOptions)) {
+        createOption({ command: program, optionProps: option });
+    }
+    Object.values(cli_props_1.commands).forEach((command) => createCommand({ commandProps: command, program }));
+    program.parse();
+};
+exports.createCli = createCli;
+const createCommand = (props) => {
+    const { commandProps, program } = props;
+    const { command: commandName, description, options, action } = commandProps;
+    const cmd = program.command(commandName).description(description);
+    if (action)
+        cmd.action(action);
+    Object.values(options).forEach((optionProps) => createOption({ optionProps, command: cmd }));
+};
+const createOption = (props) => {
+    const { optionProps, command } = props;
+    const { command: optionCommand, argument, short, description, default: defaultValue, isRequired, } = optionProps;
+    const shortChain = short
+        ? `${Array.isArray(short) ? short.join(', ') : short}, `
+        : '';
+    const argumentChain = argument ? ` ${argument}` : '';
+    command[isRequired ? 'requiredOption' : 'option'](`${shortChain}${optionCommand}${argumentChain}`, description
+        ? description + `${isRequired ? ' (required option)' : ''}`
+        : undefined, defaultValue ?? undefined);
+};
---
+++ bin/commands.js
@@ -0,0 +1,98 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.decryptCommand = exports.encryptCommand = exports.keyerCommand = void 0;
+const node_readline_1 = require("node:readline");
+const node_fs_1 = require("node:fs");
+const _1 = require(".");
+const cli_props_1 = require("./cli-props");
+const files_helper_1 = require("./helpers/files.helper");
+// Keyer script
+const keyerCommand = (props) => {
+    const { encryptRoute, encryptedRoute, decryptedRoute } = props ?? {};
+    const rl = (0, node_readline_1.createInterface)({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    const encryptFile = encryptRoute ?? cli_props_1.defaultFiles.encryptRoute;
+    const encryptedFile = encryptedRoute ?? cli_props_1.defaultFiles.encryptRoute;
+    const decryptedFile = decryptedRoute ?? cli_props_1.defaultFiles.decryptedRoute;
+    // Question for kind of crypto
+    rl.question('Kind of crypto - encrypt(e) or decrypt(d): ', (keyKindCrypto) => {
+        if (!keyKindCrypto ||
+            !['e', 'd'].some((resp) => resp === keyKindCrypto))
+            throw new Error('Kind of crypto is required');
+        const kindCrypto = keyKindCrypto === 'e' ? 'encrypt' : 'decrypt';
+        // Question for secret salt
+        rl.question(`Enter Secret Salt for ${kindCrypto}: `, (salt) => {
+            if (keyKindCrypto === 'e') {
+                (0, exports.encryptCommand)({
+                    file: encryptFile,
+                    output: encryptedFile,
+                    salt,
+                });
+                rl.close();
+            }
+            else if (keyKindCrypto === 'd') {
+                rl.question(`Do you want create output?(y/n): `, (resp) => {
+                    if (!resp || !['y', 'n'].some((resp) => resp === resp))
+                        throw new Error("Response must have 'y' or 'n'");
+                    (0, exports.decryptCommand)({
+                        file: encryptedFile,
+                        output: decryptedFile,
+                        salt,
+                        createOutput: resp === 'y' ? true : false,
+                    });
+                    rl.close();
+                });
+            }
+        });
+    });
+};
+exports.keyerCommand = keyerCommand;
+const encryptCommand = (props) => {
+    const { file, output, salt } = props;
+    if (!salt)
+        throw new Error('Salt is required');
+    console.log(`Encrypting file ${file}...`);
+    // Encrypt envs and create hash file
+    const envs = (0, node_fs_1.readFileSync)(file, 'utf-8');
+    const encrypted = (0, _1.encryptAny)({
+        secretSalt: salt,
+        toEncrypt: envs,
+        showLog: false,
+    });
+    // Verfiy if file exist
+    (0, files_helper_1.createFolderAndFile)(output);
+    // Create hash file
+    (0, node_fs_1.writeFileSync)(output, encrypted, {
+        encoding: 'utf-8',
+        flag: 'w',
+    });
+    console.log('Encrypted in ', output);
+};
+exports.encryptCommand = encryptCommand;
+const decryptCommand = (props) => {
+    const { file, output, salt, createOutput = false } = props;
+    if (!salt)
+        throw new Error('Salt is required');
+    console.log(`Decrypting file ${file}...`);
+    const hash = (0, node_fs_1.readFileSync)(file, 'utf-8');
+    const decryptedVar = (0, _1.decryptAny)({
+        secretSalt: salt,
+        toDecrypt: hash,
+        showLog: false,
+    });
+    if (!!output && createOutput) {
+        // Verfiy if file exist
+        (0, files_helper_1.createFolderAndFile)(output);
+        // Create env file
+        (0, node_fs_1.writeFileSync)(output, decryptedVar, {
+            encoding: 'utf-8',
+            flag: 'w',
+        });
+        console.log('Decrypted in ', output);
+    }
+    else
+        console.log('Decrypted data: ', decryptedVar); // Decrypt hash file and show envs in console
+};
+exports.decryptCommand = decryptCommand;
