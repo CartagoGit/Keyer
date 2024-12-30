d3da6f0807b93f672c728f9c01dd3a77d7d27520
Cartago <cartago.relaxingcup@gmail.com>
2024-12-30 22:13:40 +0100

    Mejoramos la seguridad y el algoritmo de seguridad de cifrado

--- src/decrypt.ts
+++ src/decrypt.ts
@@ -1,6 +1,7 @@
-import { scryptSync, createDecipheriv } from 'node:crypto';
+import { scryptSync, createDecipheriv, CipherGCMTypes } from 'node:crypto';
 import { reviver } from './helpers/json.helper';
 import { isValidType } from './constants/common.constant';
+import { gunzipSync } from 'node:zlib';
 
 export const decrypt = (props: {
 	toDecrypt: string;
@@ -8,18 +9,24 @@ export const decrypt = (props: {
 	showLog?: boolean;
 }): string => {
 	const { toDecrypt, secretSalt, showLog = true } = props;
-	const [iv, encrypted] = toDecrypt.split(':');
-	const key = scryptSync(secretSalt, 'sal', 32);
+	const [salt, ivBase64, authTagBase64, encryptedBase64] = toDecrypt.split(':');
+	if(!salt || !ivBase64 || !authTagBase64 || !encryptedBase64) throw new Error('Invalid format');
+	const iv = Buffer.from(ivBase64, 'base64');
+	const authTag = Buffer.from(authTagBase64, 'base64');
+    const encrypted = Buffer.from(encryptedBase64, 'base64');
+	const key = scryptSync(secretSalt, salt, 32);
+	const algorithm: CipherGCMTypes = 'aes-256-gcm';
 	const decipher = createDecipheriv(
-		'aes-256-cbc',
+		algorithm,
 		key,
-		Buffer.from(iv, 'hex')
+		iv
 	);
+	decipher.setAuthTag(authTag);
 	const decrypted = Buffer.concat([
-		decipher.update(Buffer.from(encrypted, 'hex')),
+		decipher.update(encrypted),
 		decipher.final(),
 	]);
-	const result = decrypted.toString();
+	const result = gunzipSync(decrypted).toString();
 	if (showLog) console.log('Text decrypted: ', result);
 	return result;
 };
--- src/encrypt.ts
+++ src/encrypt.ts
@@ -1,6 +1,12 @@
-import { createCipheriv, randomBytes, scryptSync } from 'node:crypto';
+import {
+	CipherGCMTypes,
+	createCipheriv,
+	randomBytes,
+	scryptSync,
+} from 'node:crypto';
 import { replacer } from './helpers/json.helper';
 import { isValidType } from './constants/common.constant';
+import { gzipSync } from 'node:zlib';
 
 export const encrypt = (props: {
 	toEncrypt: string;
@@ -8,11 +14,20 @@ export const encrypt = (props: {
 	showLog?: boolean;
 }): string => {
 	const { toEncrypt, secretSalt, showLog = true } = props;
-	const key = scryptSync(secretSalt, 'sal', 32);
+	const compressed = gzipSync(toEncrypt);
+	const salt = randomBytes(16).toString('base64');
+	const key = scryptSync(secretSalt, salt, 32);
 	const iv = randomBytes(16);
-	const cipher = createCipheriv('aes-256-cbc', key, iv);
-	const encrypted = Buffer.concat([cipher.update(toEncrypt), cipher.final()]);
-	const result = `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+	const algorithm: CipherGCMTypes = 'aes-256-gcm';
+	const cipher = createCipheriv(algorithm, key, iv);
+	const encrypted = Buffer.concat([
+		cipher.update(compressed),
+		cipher.final(),
+	]);
+	const authTag = cipher.getAuthTag();
+	const result = `${salt}:${iv.toString('base64')}:${authTag.toString(
+		'base64'
+	)}:${encrypted.toString('base64')}`;
 	if (showLog) console.log('Text encrypted: ', result);
 	return result;
 };
